6717448034f5755368efc737abbdcf611fbed98e
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-10 20:02:54 -0500

    commit tree 3ab5b2f90ee2a44f38e2ce762848002731ac458b

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20201211005747) do
 
   create_table "passengers", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
